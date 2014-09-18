39448ec1fdfefa3e910a34838ffda14ba4b68bcc
budparr <budparr@sonnetmedia.net>
2014-09-18 14:04:17 -0400

    add image and fixed mobile

--- assets/js/concatenate-scripts.js
+++ assets/js/concatenate-scripts.js
@@ -1,7 +1,25 @@
 ---
 ---
 
-var sections = $('.section'),
+{% include jquery-2.1.1.min.js %}
+
+
+$(document).ready(function() {
+  var menu = $('.centered-navigation-menu');
+  var menuToggle = $('.centered-navigation-menu-button');
+  var signUp = $('.sign-up');
+
+  $(menuToggle).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle(function(){
+      if(menu.is(':hidden')) {
+        menu.removeAttr('style');
+      }
+    });
+  });
+});
+
+var sections = $('section'),
     links = $('.link'),
     lis = $('.links > li');
 
